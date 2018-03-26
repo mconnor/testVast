70a63ccb1539ecdb5f5557a168561cef451e8109
mike connor <mike@rocketnumber9.org>
2018-03-26 14:16:42 -0500

    add test file for new L2

---
+++ docs/64564.js
@@ -0,0 +1,54 @@
+BAP.copyJSON({
+    "data": {
+      "icon_position": "top-right",
+      "default_icon": "_us",
+      "mobile_in_app_url": "",
+      "default_footer": "Privacy Controls by Ghostery, Inc.",
+      "icon_display": "normal",
+      "icon_grayscale": 30,
+      "container_opacity": 70,
+      "offset_x": 0,
+      "offset_y": 0,
+      "generic_icon": false,
+      "icon_delay": 0,
+      "nid": 64564,
+      "nwid": null,
+      "aid": 242,
+      "icid": null,
+      "skip_L2": false,
+      "behavioral": "definitive",
+      "generic_text": null,
+      "adv_name": "Ghostery",
+      "adv_msg": "Custom",
+      "adv_logo": "http://dev.betrad.com/a/ui/logos/242/5d3e82ee8e0a4e349dc010e0218f1fcf.png",
+      "adv_link": "http://www.evidon.com",
+      "mobile_message": null,
+      "display_mobile_overlay": false,
+      "mobile_advertiser_logo_url": "",
+      "default_icon_text": "AdChoices",
+      "default_generic1": "This ad has been matched to your interests. It was selected for you based on your browsing activity.",
+      "default_generic2": "This ad may have been matched to your interests based on your browsing activity.",
+      "default_generic3": "helped",
+      "default_generic4": "determine that you might be interested in an ad like this.",
+      "default_generic5": "select this ad for you.",
+      "default_generic6": "selected this ad for you.",
+      "default_link1": "More information & opt-out options",
+      "default_link2": "What is interest based advertising",
+      "default_link3": "Learn about your choices",
+      "ecid": null,
+      "hide_wi": false,
+      "hide_cl": false,
+      "server": [
+        {
+          "name": "12Mnkys"
+        }
+      ],
+      "message_properties": {
+        "skip_L2_de": true,
+        "translation_de": {
+          "icon": "_de",
+          "icon_text": "Datenschutzinfo"
+        }
+      }
+    }
+  });
