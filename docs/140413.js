93c4f0a72d8fb190fcf72f1f66622eaafaa03d7f
mike connor <mike@rocketnumber9.org>
2019-08-06 13:11:00 -0500

    testing Link 3 - learn…

---
+++ docs/140413.js
@@ -0,0 +1,48 @@
+BAP.copyJSON({
+    "data": {
+      "icon_position": "top-right",
+      "default_icon": "_us",
+      "mobile_in_app_url": "",
+      "default_footer": "Privacy Controls by Evidon, Inc.",
+      "icon_display": "normal",
+      "icon_grayscale": 30,
+      "container_opacity": 70,
+      "offset_x": 0,
+      "offset_y": 0,
+      "generic_icon": false,
+      "icon_delay": 0,
+      "nid": 140413,
+      "nwid": null,
+      "aid": 4996,
+      "icid": null,
+      "skip_L2": false,
+      "behavioral": "definitive",
+      "generic_text": null,
+      "adv_name": "August 25 2017 Test Company",
+      "adv_msg": "",
+      "adv_logo": "http://staging.betrad.com/a/ui/logos/4996/bd0df1c7ba814796accb9eda2988e67d.png",
+      "adv_link": "",
+      "mobile_message": null,
+      "display_mobile_overlay": false,
+      "mobile_advertiser_logo_url": "",
+      "default_icon_text": "AdChoices",
+      "default_generic1": "This ad has been matched to your interests. It was selected for you based on your browsing activity.",
+      "default_generic2": "This ad may have been matched to your interests based on your browsing activity.",
+      "default_generic3": "helped",
+      "default_generic4": "determine that you might be interested in an ad like this.",
+      "default_generic5": "select this ad for you.",
+      "default_generic6": "selected this ad for you.",
+      "default_link1": "More information & opt-out options",
+      "default_link2": "What is interest based advertising",
+      "default_link3": "Learn about your choices",
+      "ecid": null,
+      "hide_wi": false,
+      "hide_cl": false,
+      "server": [
+        {
+          "name": "\t Wap.co.uk"
+        }
+      ],
+      "message_properties": {}
+    }
+  });
